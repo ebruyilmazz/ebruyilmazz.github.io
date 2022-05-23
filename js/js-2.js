1378bb14c82b06f3735728a27fd19c44ee5b05e4
ebruyilmazz <103778178+ebruyilmazz@users.noreply.github.com>
2022-05-23 13:43:53 +0300

    first

--- js/js-2.js
+++ js/js-2.js
@@ -56,3 +56,24 @@ uyarı2();
 
 
 
+var kategoriler=new Array();
+kategoriler[0]="Korku";
+kategoriler[1]="Edebiyat";
+kategoriler[2]="Bilim";
+kategoriler[2]="Felsefe";
+kategoriler[3]="Kişisel Gelişim";
+
+function kategori(){
+    document.write(kategoriler[0]);
+    document.write("<br>"+"1-Stephen King – Hayvan Mezarlığı" + "<br>" +" 2-Howard Phillips Lovecraft – Deliliğin Dağlarında" +" <br>"+ "3-Josh Malerman – Kafes" + "<br>" + "4-Stephen King – Mahşer" + "<br>" +"5-Mary Shelley – Frankenstein"+" <br>" +" 6-Adam Nevill – Daire 16"+ "<br>"+ "William Peter Blatty – Şeytan" + "<br>"+"<br>")
+    document.write(kategoriler[1]);
+    document.write("<br>"+"1-Şeker Portakalı ,Jose Mauro De Vasconcelos" + "<br>" +" 2-Saatleri Ayarlama Enstitüsü,Ahmet Hamdi Tanpınar" +" <br>"+ "3-İnce Memed 1, (1955) Yaşar Kemal" + "<br>" + "4-Anayurt Oteli, (1973) Yusuf Atılgan" + "<br>" +"5-Araba Sevdası, (1898) Recâizâde Mahmut Ekrem"+" <br>" +" 6-Benim Adım Kırmızı, (1998) Orhan Pamuk"+ "<br>"+"<br>")
+    document.write(kategoriler[2]);
+    document.write("<br>"+"1-Büyük Sorulara Kısa Yanıtlar,Stephen Hawking" + "<br>" +" 2-Zamanın Kısa Tarihi, Stephen Hawking" +" <br>"+ "3-Bilimin Büyüsü ,A. M. Celal Şengör" + "<br>" + "4-Aforizmalar, Stephen Hawking" + "<br>"+"<br>")
+    document.write(kategoriler[3]);
+    document.write("<br>"+"1-İnsan Geleceğini Nasıl Kurar?, İlber Ortaylı" + "<br>" +" 2-Bir Ömür Nasıl Yaşanır?, İlber Ortaylı" +" <br>"+ "3-Var mısın? - Güçlü Bir Yaşam İçin Öneriler, Doğan Cüceloğlu" + "<br>"+"<br>") 
+}
+
+
+
+
